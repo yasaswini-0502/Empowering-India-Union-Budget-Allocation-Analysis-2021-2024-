98a19b041ebe657a50ca66fa4598dae3ec06cbc2
yasaswini-0502 <kyasaswini11@gmail.com>
2026-02-19 19:37:19 +0530

    Add files via upload

---
+++ union_budget_flask_project/app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template("index.html")
+
+@app.route('/dashboard')
+def dashboard():
+    return render_template("dashboard.html")
+
+@app.route('/story')
+def story():
+    return render_template("story.html")
+
+if __name__ == '__main__':
+    app.run(debug=True)
